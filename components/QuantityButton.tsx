55469d8a6259be8effbd5b2fa696b2d20a65a663
Shek Rasel <your-email@example.com>
2025-04-02 21:50:23 +0600

    make it functional

--- components/QuantityButton.tsx
+++ components/QuantityButton.tsx
@@ -2,23 +2,53 @@ import React from 'react'
 import { Button } from './ui/button';
 import { Minus, Plus } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import useCartStore from '@/store';
+import { Product } from '@/sanity.types';
+import toast from 'react-hot-toast';
+
+interface Props {
+  product  : Product;
+  className? : string;
+}
 
 
+function QuantityButton({product,}:Props) {
 
-function QuantityButton() {
+  const {addItem,getItemCount,removeItem} =  useCartStore();
+  const itemCount = getItemCount(product?._id);
+  const isOutStock = product?.stock === 0;
 
-    const quantity  = 1 ;
+  const handleRemoveProduct = ()=>{
+    removeItem(product?._id);
+    if(itemCount > 1){
+
+      toast.success('Quantity discrease successfully');
+    }else{
+      toast.success(`${product?.name?.substring(0,12)} removed sussessfuly`)
+    }
+  };
+
     
   return (
     <div className='flex  justify-center items-center'>
-        <Button  variant={'outline'} className={cn('font-base py-0 size-6')}>
+        <Button 
+          onClick={handleRemoveProduct}
+          disabled={itemCount === 0 || isOutStock}
+        variant={'outline'} className={cn('font-base py-0 size-6 cursor-pointer')}>
             <Minus/>
         </Button>
         <span className=' px-2 font-semibold'>
-                {quantity}
+                {itemCount}
         </span>
 
-        <Button variant={'outline'}  className={cn('font-base py-0 size-6')}>
+        <Button
+        onClick={() => {
+          addItem(product);
+          toast.success(
+            `${product?.name?.substring(0, 12)}... added successfully!`
+          );
+        }}
+        variant={'outline'}  className={cn('font-base py-0 size-6 cursor-pointer')}>
             <Plus/>
         </Button>
     </div>
