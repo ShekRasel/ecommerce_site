d40502010c37ffe644f6a350b031e0c4cde68488
Shek Rasel <your-email@example.com>
2025-04-03 11:53:30 +0600

    dynamically product added

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -1,33 +1,50 @@
 "use client";
-import { HeaderMenuData } from "@/constant";
+// import { HeaderMenuData } from "@/constant";
+import { CATEGORIES_QUERYResult } from "@/sanity.types";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
-function HeaderMenu() {
+function HeaderMenu({categories}:{categories:CATEGORIES_QUERYResult | undefined}) {
   const path = usePathname();
 
   return (
     <div className={`lg:inline-flex hidden gap-5`}>
-      {HeaderMenuData?.map((item) => (
-        <Link
-          href={item.link}
-          key={item.title}
-          className={`hover:text-black hoverEffect font-semibold relative group ${
-            path === item?.link && "text-red-400"
-          }`}
-        >
-          {item.title}
-
-          <span
+      <Link href={'/'} className={`hover:text-black hoverEffect  relative group ${
+            path === '/' ? "text-darkColor font-bold" : 'font-semibold'
+          }`}>
+        Home
+        <span
             className={`absolute -bottom-0.5 left-1/2 h-0.5 bg-darkColor hoverEffect   w-0  group-hover:w-1/2 group-hover:left-0 ${
-              path === item?.link && "w-1/2"
+              path === '/' && "w-1/2"
             }`}
           />
           <span />
           <span
             className={`absolute -bottom-0.5 right-1/2 h-0.5 bg-darkColor hoverEffect   w-0  group-hover:w-1/2 group-hover:right-0 ${
-              path === item?.link && "w-1/2"
+              path === '/' && "w-1/2"
+            }`}
+          />
+      </Link>
+      {categories?.map((category) => (
+        <Link
+          href={`/category/${category?.slug?.current}`}
+          key={category?._id}
+          className={`hover:text-black hoverEffect relative group ${
+            path === `/category/${category?.slug?.current}` ? "text-darkColor font-bold" : 'font-semibold'
+          }`}
+        >
+          {category.title}
+
+          <span
+            className={`absolute -bottom-0.5 left-1/2 h-0.5 bg-darkColor hoverEffect   w-0  group-hover:w-1/2 group-hover:left-0 ${
+              path === `/category/${category?.slug?.current}` && "w-1/2"
+            }`}
+          />
+          <span />
+          <span
+            className={`absolute -bottom-0.5 right-1/2 h-0.5 bg-darkColor hoverEffect   w-0  group-hover:w-1/2 group-hover:right-0 ${
+              path === `/category/${category?.slug?.current}` && "w-1/2"
             }`}
           />
           <span />
