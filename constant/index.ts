1c3405172f210f07a5da8b56ef5479b9b498c918
Shek Rasel <your-email@example.com>
2025-04-06 01:43:56 +0600

    static value delele

--- constant/index.ts
+++ constant/index.ts
@@ -1,11 +1,5 @@
 export const HeaderMenuData = [
     {title : 'Home', link : '/'},
-    {title : 'Featured', link : '/featured'},
-    {title : 'New', link : '/new'},
-    {title : 'Accessories', link : '/accessories'},
-    {title : 'T-Shirt', link : '/t-shirt'},
-    {title : 'Men', link : '/men'},
-    {title : 'Women', link : '/women'},
 ]
 
 export const productType = [
