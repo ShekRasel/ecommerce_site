a6532ce58b8ad5cd2c112e2a1804f32e0959504b
Shek Rasel <your-email@example.com>
2025-04-06 01:43:40 +0600

    w-full width add

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -6,7 +6,7 @@ import { Loader2 } from "lucide-react";
 
 const Loading = () => {
   return (
-    <div className="fixed min-h-screen w-ful bg-white left-0 top-0 flex items-center justify-center z-50">
+    <div className="fixed min-h-screen w-full bg-white left-0 top-0 flex items-center justify-center z-50">
       <div className="flex flex-col justify-center items-center gap-1">
         <Logo className="">Synzo</Logo>
         <motion.div animate={{scale: [1,1.1,1]}} transition={{repeat:Infinity, duration : 1.5}} className="flex items-center space-x-2 text-green-800">
